687afd2e73dc4e9737aac2f219d153d41b9264c0
RenatoOliva003 <renatoliva9@gmail.com>
2026-06-11 12:46:46 -0400

    fix: portal abre correctamente desde /portal?token= ademas de /movil/{token}

--- index.tsx
+++ index.tsx
@@ -9,11 +9,15 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const tokenMatch = window.location.pathname.match(/^\/movil\/([a-f0-9-]{36})$/i);
+const pathToken  = window.location.pathname.match(/^\/movil\/([a-f0-9-]{36})$/i)?.[1];
+const queryToken = window.location.pathname === '/portal'
+  ? new URLSearchParams(window.location.search).get('token') ?? undefined
+  : undefined;
+const portalToken = pathToken ?? queryToken;
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    {tokenMatch ? <ConductorPortal token={tokenMatch[1]} /> : <App />}
+    {portalToken ? <ConductorPortal token={portalToken} /> : <App />}
   </React.StrictMode>
 );
